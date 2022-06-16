1d6d1d3561c522e3cb655899e2b4e66c854e522a
Luis Galindo <egalindo54@uabc.edu.mx>
2022-06-16 14:19:52 -0700

    plantillas funcionando

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import string
 import markdown
 import os
 
 input_folder = "./content"
 output_folder = "./output"
+
+# Aqui va la esstructura del html, el contenido lo genera mdt2html
+templates_folder = "./templates"
+
+#
+styles_folder = "./styles"
+scripts_folder = "./script"
 
 
 def file_names() -> list:
@@ -10,28 +18,53 @@ def file_names() -> list:
     return os.listdir(input_folder)
 
 
-def remocesuffux(file_name: str) -> str:
+def removesuffix(file_name: str) -> str:
     """Elimina el suffijo del archivo"""
     return file_name.rsplit('.', 1)[0]
 
 
-def md2html(inputf: str, outputf: str):
-    """ Convertir archivo .md a .html """
-    with open(inputf, 'r') as f:
-        text = f.read()
-        html = markdown.markdown(text)
+def removeprefix(file_anme: str) -> str:
+    """Elimina el prefijo del archivo"""
+    return file_name.rsplit('.', 1)[1]
 
-    with open(outputf, 'w') as f:
-        f.write(html)
+
+def filltemplate(template_path: str, param: dict) -> str:
+    template = string.Template(load(template_path))
+    return template.substitute(param)
+
+
+def load(file_path: str) -> str:
+    """carga el archivo"""
+    with open(file_path, 'r') as f:
+        text = f.read()
+    return text
+
+
+def save(content: str, file_path: str):
+    """Guarda el archivo"""
+    with open(file_path, 'w') as f:
+        f.write(content)
+
+
+def md2html(md_text: str):
+    """Convertir archivo .md a .html"""
+    return markdown.markdown(md_text)
 
 
 def generate_site():
     for file_name in file_names():
-        inputf = input_folder + "/" + remocesuffux(file_name) + ".md"
-        outputf = output_folder + "/" + remocesuffux(file_name) + ".html"
+        inputf = input_folder + "/" + removesuffix(file_name) + ".md"
+        outputf = output_folder + "/" + removesuffix(file_name) + ".html"
         print(inputf)
         print(outputf)
-        md2html(inputf, outputf)
+
+        md_text = load(inputf)
+        html_text = filltemplate(templates_folder + "/" + "root.html", {
+            "title": removesuffix(file_name),
+            "content": md2html(md_text)
+        })
+
+        save(html_text, outputf)
 
 
 # def generate_site():
