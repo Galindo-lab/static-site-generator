691a9276fe30f64db90ea74f2a2c5b9f679ffc06
Luis Galindo <egalindo54@uabc.edu.mx>
2022-06-19 12:25:43 -0700

    encuentra el path a la plantilla

--- generator.py
+++ generator.py
@@ -1,45 +1,62 @@
-
 import os, string, json
-import markdown  
+import markdown
 
+# Cargar el archivo proyect.json
 with open("proyect.json") as json_file:
     data = json.load(json_file)
-        
-proyect_name = data["proyectName"]         # nombre del proyecto
-input_dir = data["inputFolder"]            # directorio de entrada
-output_dir = data["outputFolder"]          # directorio de salida
-templates_dir = data["templatesFolder"]    # directorio de plantillas
-default_template = data["defaultTemplate"] # plantilla default
-sections = data["sections"]                # secciones
+
+# Variables globales del proyecto
+proyect_name = data["proyectName"]  # nombre del proyecto
+input_dir = data["inputFolder"]  # directorio de entrada
+output_dir = data["outputFolder"]  # directorio de salida
+templates_dir = data["templatesFolder"]  # directorio de plantillas
+default_template = data["defaultTemplate"]  # plantilla default
+sections = data["sections"]  # secciones
+
 
 def generate():
-    # directorios de entrada
-    input_dirs = input_path_list()
+    input_dirs = input_path_list()  # Directorios de entrada
+    output_dirs = output_path_list()  # Directorios de salida
+    # input_files = input_files_paths()  #
+    # output_files = output_files_paths()  #
 
     # crea los directorios de entrada si no existen
+    # cada direcotrio correponde a una seccion
     for directory in input_dirs:
         make_dir(directory)
 
-    # Directorios de salida
-    output_dirs = output_path_list()
-    # crea los directorios de entrada si no existen
+    # crea los directorios de salida si no existen
     for directory in output_dirs:
         make_dir(directory)
 
+    for section in sections:
+        # usar la plantilla default si no hay plantilla para
+        # la seccion 
+        template_path = template(section) if template_exist(
+            section) else default_templat
         
 
 def input_path_list() -> list:
     """Retorna la ruta de entrada para cada seccion del proyecto"""
     return [f"{input_dir}/{s}" for s in sections]
 
+
 def output_path_list() -> list:
     """Retorna la ruta de salida para cada seccion del proyecto"""
     return [f"{output_dir}/{s}" for s in sections]
-    
+
 
 def make_dir(directory: list):
     """Crea un directorio"""
     os.makedirs(directory, exist_ok=True)
+
+
+def template(section: str) -> str:
+    return f"{templates_dir}/{section}.html"
+
+
+def template_exist(section: str) -> bool:
+    return os.path.exists(template(section))
 
 
 def remove_suffix(file_name: str) -> str:
