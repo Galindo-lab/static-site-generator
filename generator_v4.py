84012337a600ca3cef7014e2b1d5002b8a3672b7
Luis Galindo <egalindo54@uabc.edu.mx>
2022-06-30 23:58:35 -0700

    documentación agregada

--- generator_v4.py
+++ generator_v4.py
@@ -9,6 +9,11 @@ from pathlib import Path
 import markdown
 
 
+
+
+
+
+
 class Article():
     name: str
     section: str
@@ -19,6 +24,7 @@ class Article():
         # ningun archivo con '-' debe existir en el directorio de salida,
         # recuerda '-' al inicio del nombre del archivo siginifica que el
         # articulo no esta publicado
+        # 
         # origen: https://stackoverflow.com/a/4945578
         self.name = name.lstrip("-")
         self.section = section
@@ -26,8 +32,12 @@ class Article():
         self.destination_path = Path(destination, section,self.name).with_suffix(".html")
 
     def template_data(self, content: str) -> dict:
-        keys = ["date", "title", "autor", "content"]
-        values = self.name.split("-").append(content)
+        """
+        separar el nombre del archivo en sus componentes
+        """
+        keys = ["date", "title", "author", "content"]
+        values = self.name.split("-")
+        values.append(content)
         data = dict(zip(keys, values))
         
         return data
@@ -35,14 +45,15 @@ class Article():
     def load_template(self) -> Template:
         default = Path("templates", "default").with_suffix(".html")
         template = Path("templates", self.section).with_suffix(".html")
-        print(default, template)
-        
         if not template.exists():
             return Template(default.read_text())
 
         return Template(template.read_text())
         
     def publish(self):
+        """
+        crear el archivo .html del articulo
+        """
         md_text = self.origin_path.read_text()
         html_text = markdown.markdown(md_text)
         template = self.load_template()
@@ -51,11 +62,36 @@ class Article():
         self.destination_path.write_text(output)
 
     def unpublish(self):
+        """
+        eliminar archivo .html del articulo
+        """
         if self.destination_path.exists():
             self.destination_path.unlink()
 
     def is_published(self) -> bool:
+        """
+        Revisar si tiene el caracter '-' al inicio del 
+        nombre
+        """
         return self.origin_path.name[0] != '-'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class Proyect():
@@ -75,6 +111,12 @@ class Proyect():
         self.unpublished = self.data["unpublished-sections"]
 
     def update_folders(self):
+        """
+        Crea todos los directorios requeridos para trabajar los 
+        el proyecto, si se despublica una seccion se elimina
+        el directorio 
+        """
+        
         # eliminar folders de secciones despublicadas
         for section in self.unpublished:
             directory = Path(self.destination, section)
@@ -87,6 +129,10 @@ class Proyect():
             directory.mkdir(exist_ok=True)
 
     def update_files(self):
+        """
+        Actualiza los archivos html, si cambia su contenido 
+        actualiza el archivo y si se despublica se elimina.
+        """
         for section in self.sections:
             section_path = Path(self.origin, section)
             for article_path in section_path.glob("*.md"):
