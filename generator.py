68ed111767f7055ca935a88bd876c7a77fa8cac0
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-06-17 12:21:57 -0700

    Genera html en base a plantillas

--- generator.py
+++ generator.py
@@ -1,100 +1,67 @@
-
-import string
-import json
-import shutil
-import markdown
-
-from string import Template
+import sys
 from pathlib import Path
 
-
-f = open('proyect.json',)
-proyect = json.load(f)
-
-base_directory = Path(proyect["base-directory"])
-publishing_directory = Path(proyect["publishing-directory"])
-template_directory = Path(proyect["templates-directory"])
+import jinja2
+import markdown
 
 
-def list_md_files(directory: Path) -> list:
-    """
-    Iterar el directorio base para extraer los archivos '.md'.
-    """
-    return [diir for diir in base_directory.glob("**/*.md")]
+# configuraciones
+MD_ENCODING = "utf-8"
 
+# con la extencion de metadatos
+md = markdown.Markdown(extensions=['meta'])
 
-def publish_file(input_file: Path) -> Path:
-    """
-    Convierte un archivo de el directorio base a el directorio 
-    de publicación.
-    """
-    # eliminar el directorio base
-    foo = input_file.relative_to(base_directory)
-    # archivo equivalente en publishing_directory
-    output_file = publishing_directory / foo.with_suffix(".html")
-    return output_file
+# identificador de argumentos
+SCRIPT_NAME = 0
+FILE_PATH = 1
 
+# numero minimo de argumentos
+MIN_ARGS = 2
 
-def load_template(name: str) -> Template:
-    """
-    Carga las plantillas 
-    """
-    template = template_directory / f"{name}.html"
-    return Template(template.read_text())
-    
+if __name__ == '__main__':
+    if len(sys.argv) < MIN_ARGS:
+        print("Archivo invalido")
+        sys.exit(0)
 
-def create_dirs():
-    """
-    Crea los directorios necesario para crear el export 
-    """
-    for diir in list_md_files(base_directory):
-        folder = publish_file(diir).parent
-        folder.mkdir(exist_ok=True)
-        
+    # Cargar los archivos
+    # -------------------
+    path = Path(sys.argv[FILE_PATH])
 
-def extract_meta(input_file: Path) -> dict: 
-    """
-    Extraer los metadatos de el documento.
-    """
-    md = markdown.Markdown(extensions=['meta'])
-    file_path = str(input_file.resolve()) # Path to string
-    md_meta = md.convertFile(file_path).Meta
-    return {k:v[0] for (k,v) in md_meta.items() }
-    
+    if path.is_file() == False:
+        # validar no si existe el archivo
+        print(f'"{path}" no es un archivo')
+        sys.exit(0)
 
-def export_file(input_file: Path):
-    """
-    Aplicar la plantilla y escribir el archivo '.html'
-    """
-    output_file = publish_file(input_file)
-    md = markdown.Markdown(extensions=['meta'])
+    if path.suffix.lower() != ".md":
+        # validar que es un archivo markdown
+        print(f'"{path}" no es un archivo markdown')
+        sys.exit(0)
 
-    meta = extract_meta(input_file)
-    md_text = input_file.read_text()
+    # cargar las plantillas
+    # ---------------------
+    template_loader = jinja2.FileSystemLoader("./templates")
+    template_env = jinja2.Environment(loader=template_loader)
+    template = template_env.get_template("default.html")
+
+    # convertir md a html
+    # -------------------
+    md_text = path.read_text(encoding=MD_ENCODING)
     html_text = md.convert(md_text)
 
-    template_name = meta["template"] if "template" in meta else "default"
-    template = load_template(template_name)
-    
-    output_file.write_text(template.substitute({
-        "date" : meta["date"],
-        "title" : meta["title"],
-        "author" : meta["authors"],
-        "content" : html_text
-    }))
-    
+    # crear el diccionario de carga
+    # -----------------------------
+    context = {"content": html_text}
+
+    for key, value in md.Meta.items():
+        context[key] = value[0] if len(value) < 2 else value
+
+    # generar el documento html con plantilla
+    # ---------------------------------------
+    output_text = template.render(context)
     
 
-def export_files():
-    """
-    Exportar los archivos.
-    """
-    for filee in list_md_files(base_directory):
-        export_file(filee)
-        
-
-
-print(" *** Generador *** ")
-create_dirs()
-export_files()
-print(" ****** End ****** ")
+    # escribir el archivo de salida
+    # -----------------------------
+    f = open(path.with_suffix(".html"), "r+")
+    f.write(output_text)
+    f.close()
---
+++ markdown/test.md
@@ -0,0 +1,16 @@
+---
+title: this is the title and it is compulsory
+tags: this part is optional
+something: this is not interesting, only 'title' and 'tags' is
+date: 20-06-2023
+author: Luis E. Galindo
+author: Juan perez
+---
+
+# titulo
+Et consequat deserunt proident nulla velit. Eu eiusmod esse pariatur duis. Ipsum id cillum adipisicing id ex in eiusmod labore. Sunt ipsum laboris do voluptate adipisicing deserunt. Esse laborum laboris velit ut sit commodo anim excepteur. Adipisicing id reprehenderit exercitation proident et magna quis.
+
+## subtitulo 
+![alt](./mdtf.png)
+
+### subsubtitulo
